3fd8bc27d2294c395ce6bd91761468d3ad29bb1a
arlind <arlind12344@gmail.com>
2023-07-13 22:34:26 +0200

    db Dump

--- cars.sql
+++ cars.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2022 at 12:01 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Jul 13, 2023 at 10:33 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `active_sessions`
+--
+
+CREATE TABLE `active_sessions` (
+  `uid` int(11) NOT NULL,
+  `session_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `active_sessions`
+--
+
+INSERT INTO `active_sessions` (`uid`, `session_id`) VALUES
+(2, 'uhlaht9et5g4toiitu34g5evf1'),
+(2, 'p701v7n7pri9qa4sdr80ljhaq0'),
+(2, 'c60mipqf02rleck7bu8jj0p9hf');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cars`
 --
 
@@ -33,15 +53,92 @@ CREATE TABLE `cars` (
   `price` int(11) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `transmission` int(11) DEFAULT NULL,
-  `available` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `available` int(11) DEFAULT NULL,
+  `order_count` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`id`, `name`, `price`, `description`, `transmission`, `available`) VALUES
-(4, 'test', 20, 'test', 0, 1);
+INSERT INTO `cars` (`id`, `name`, `price`, `description`, `transmission`, `available`, `order_count`) VALUES
+(1, 'test', 20, 'test', 0, 1, 1),
+(2, 'test 2', 25, 'test 2 desc', 1, 1, 2),
+(3, 'test 4', 25, 'test 4 desc', 1, 1, 5),
+(4, 'test 3', 20, 'test 3 desc', 1, 1, 3),
+(5, 'car 5', 600, '456+', 1, 1, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `firstname`, `lastname`, `email`, `phone_number`, `created_at`) VALUES
+(1, 'Arlind', 'Ismalaja', 'asd@asd.com', '123456789', '2023-07-09'),
+(2, 'Biorni', 'Ismalaja', 'asd@asd.com', '123456789', '2023-07-09'),
+(3, 'Test', 'Test', 'test@test.com', '1234567890', '2023-07-09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `revenue`
+--
+
+CREATE TABLE `revenue` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `rental_date` date NOT NULL DEFAULT current_timestamp(),
+  `car_id` int(11) NOT NULL,
+  `rental_duration` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `revenue`
+--
+
+INSERT INTO `revenue` (`id`, `customer_id`, `rental_date`, `car_id`, `rental_duration`, `price`) VALUES
+(1, 1, '2023-05-09', 4, 1, 10),
+(3, 3, '2023-07-19', 4, 1, 20),
+(4, 1, '2023-06-03', 4, 5, 15),
+(5, 2, '2023-07-19', 4, 1, 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `date_created` date NOT NULL DEFAULT current_timestamp(),
+  `currency` varchar(255) NOT NULL DEFAULT 'Euro',
+  `last_login` date NOT NULL DEFAULT current_timestamp(),
+  `is_admin` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `date_created`, `currency`, `last_login`, `is_admin`) VALUES
+(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2023-07-05', 'Euro', '2023-07-13', 1);
 
 --
 -- Indexes for dumped tables
@@ -54,6 +151,24 @@ ALTER TABLE `cars`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `revenue`
+--
+ALTER TABLE `revenue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,7 +176,25 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `revenue`
+--
+ALTER TABLE `revenue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
