ba18a3a909efe42c987e0b3888b8b6b6a87518a7
Arlind <arlind12344@gmail.com>
2023-07-21 19:38:10 +0200

    progress

--- admin_dashboard/views/messages/messages.js
+++ admin_dashboard/views/messages/messages.js
@@ -11,4 +11,15 @@ class Messages {
         const data = await response.json();
         return data;
     }
+
+    async fetchMessage(id) {
+        const response = await fetch(`/admin_dashboard/index.php/api/messages/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        return data;
+    }
 }
