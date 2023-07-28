be0353ed145cc2e9360a3d14d0e18170b00d7250
arlind <arlind12344@gmail.com>
2023-07-28 23:42:58 +0200

    dbDump 28.07.23

--- cars.sql
+++ cars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2023 at 10:33 PM
+-- Generation Time: Jul 28, 2023 at 11:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,15 +32,6 @@ CREATE TABLE `active_sessions` (
   `session_id` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `active_sessions`
---
-
-INSERT INTO `active_sessions` (`uid`, `session_id`) VALUES
-(2, 'uhlaht9et5g4toiitu34g5evf1'),
-(2, 'p701v7n7pri9qa4sdr80ljhaq0'),
-(2, 'c60mipqf02rleck7bu8jj0p9hf');
-
 -- --------------------------------------------------------
 
 --
@@ -52,21 +43,25 @@ CREATE TABLE `cars` (
   `name` varchar(255) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
+  `travel_distance` varchar(255) DEFAULT NULL,
   `transmission` int(11) DEFAULT NULL,
   `available` int(11) DEFAULT NULL,
-  `order_count` int(11) NOT NULL DEFAULT 0
+  `next_order` date DEFAULT NULL,
+  `order_count` int(11) NOT NULL DEFAULT 0,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`id`, `name`, `price`, `description`, `transmission`, `available`, `order_count`) VALUES
-(1, 'test', 20, 'test', 0, 1, 1),
-(2, 'test 2', 25, 'test 2 desc', 1, 1, 2),
-(3, 'test 4', 25, 'test 4 desc', 1, 1, 5),
-(4, 'test 3', 20, 'test 3 desc', 1, 1, 3),
-(5, 'car 5', 600, '456+', 1, 1, 9);
+INSERT INTO `cars` (`id`, `name`, `price`, `description`, `travel_distance`, `transmission`, `available`, `next_order`, `order_count`, `created_at`) VALUES
+(1, 'test', 20, 'test', NULL, 0, 1, '2023-07-27', 1, '2023-07-20'),
+(2, 'test 2', 25, 'test 2 desc', NULL, 1, 1, NULL, 2, '2023-07-20'),
+(3, 'test 3', 25, 'test 3 desc', NULL, 1, 1, NULL, 5, '2023-07-20'),
+(4, 'test 4', 20, 'test 4 desc', NULL, 1, 1, NULL, 3, '2023-07-20'),
+(5, 'test 5', 600, '456+', NULL, 1, 1, NULL, 9, '2023-07-20'),
+(13, 'test 6', 20, 'test 6 descr', NULL, 1, 1, NULL, 3, '2023-07-20');
 
 -- --------------------------------------------------------
 
@@ -90,7 +85,29 @@ CREATE TABLE `customers` (
 INSERT INTO `customers` (`id`, `firstname`, `lastname`, `email`, `phone_number`, `created_at`) VALUES
 (1, 'Arlind', 'Ismalaja', 'asd@asd.com', '123456789', '2023-07-09'),
 (2, 'Biorni', 'Ismalaja', 'asd@asd.com', '123456789', '2023-07-09'),
-(3, 'Test', 'Test', 'test@test.com', '1234567890', '2023-07-09');
+(4, 'Charlie', 'Brown', 'charlie@example.com', '', '2023-07-23'),
+(5, 'Bob', 'Smith', 'bob@example.com', '', '2023-07-23'),
+(8, 'TEST', 'TEST', 'TEST@example.com', '', '2023-07-26'),
+(9, 'Alice', 'Johnson', 'alice@example.com', '', '2023-07-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `data` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `data`) VALUES
+(9, '{\"senders\":[{\"name\":\"Alice\",\"email\":\"alice@example.com\",\"lastname\":\"Johnson\"}],\"data\":[{\"id\":1,\"sender\":\"userA\",\"content\":\"Hello there!\",\"timestamp\":\"2023-07-21T10:30:00\"}]}\r\n'),
+(10, '{\"senders\":[{\"name\":\"Bob\",\"email\":\"bob@example.com\",\"lastname\":\"Smith\"}],\"data\":[{\"id\":2,\"sender\":\"userB\",\"content\":\"Hi, how are you?\",\"timestamp\":\"2023-07-21T11:15:00\"}]}\r\n');
 
 -- --------------------------------------------------------
 
@@ -115,7 +132,9 @@ INSERT INTO `revenue` (`id`, `customer_id`, `rental_date`, `car_id`, `rental_dur
 (1, 1, '2023-05-09', 4, 1, 10),
 (3, 3, '2023-07-19', 4, 1, 20),
 (4, 1, '2023-06-03', 4, 5, 15),
-(5, 2, '2023-07-19', 4, 1, 30);
+(5, 2, '2023-07-19', 4, 1, 30),
+(6, 5, '2023-07-19', 4, 1, 30),
+(7, 5, '2023-07-19', 4, 3, 30);
 
 -- --------------------------------------------------------
 
@@ -138,7 +157,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `date_created`, `currency`, `last_login`, `is_admin`) VALUES
-(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2023-07-05', 'Euro', '2023-07-13', 1);
+(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2023-07-05', 'Euro', '2023-07-28', 1);
 
 --
 -- Indexes for dumped tables
@@ -154,6 +173,12 @@ ALTER TABLE `cars`
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -176,19 +201,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `revenue`
 --
 ALTER TABLE `revenue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
