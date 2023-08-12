f5d855e0ee37e7a40bc35a9ac47da66f28cd0e1c
asubaruwrxsti <arlind12344@gmailcom>
2023-08-12 18:23:51 +0200

    progress

--- admin_dashboard/views/orders/orders.js
+++ admin_dashboard/views/orders/orders.js
@@ -34,7 +34,6 @@ class Orders {
 
     async isCarBusy(carId, rentalDate, returnDate) {
         let busy = false;
-		let car = await this.getCar(carId);
 
         let revenue = await fetch(`/admin_dashboard/index.php/api/revenue/`, {
             method: 'GET',
@@ -43,12 +42,18 @@ class Orders {
             }
         });
         revenue = await revenue.json();
+        revenue.forEach(order => {
+            if (order.car_id == carId) {
+                let orderRentalDate = new Date(order.rental_date);
+                let orderReturnDate = new Date(orderRentalDate.getTime() + order.rental_duration * 24 * 60 * 60 * 1000);
 
-        return revenue.some(order => {
-            let orderRentalDate = new Date(order.rental_date);
-            let orderReturnDate = orderRentalDate.setDate(orderRentalDate.getDate() + order.rental_duration);
-			return (order.car_id == carId && orderRentalDate <= rentalDate && orderReturnDate >= returnDate);
+                if (orderRentalDate <= new Date(rentalDate) && new Date(returnDate) <= orderReturnDate) {
+                    busy = true;
+                    return;
+                }
+            }
         });
+        return busy;
     }
 
     async searchCustomer(customerId) {
@@ -73,26 +78,55 @@ class Orders {
             body: order
         });
         let data = await response.json();
-        return data;
-    }
 
-    async insertCustomer(customer) {
-        customer = JSON.stringify(Object.fromEntries(customer));
-
-        const response = await fetch(`/admin_dashboard/index.php/api/customers/`, {
+        let car = await fetch(`/admin_dashboard/index.php/api/cars/${JSON.parse(order).car_id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        car = await car.json();
+        let update = await fetch(`/admin_dashboard/index.php/api/cars/edit/${JSON.parse(order).car_id}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: customer
+            body: JSON.stringify({
+                name: car[0].name,
+                price: car[0].price,
+                description: car[0].description,
+                travel_distance: car[0].travel_distance,
+                transmission: car[0].transmission,
+                available: car[0].available,
+                next_order: new Date().getTime().setHours(0, 0, 0, 0),
+                order_count: String(parseInt(car[0].order_count) + 1),
+                created_at: car[0].created_at
+            })
+        });
+        update = await update.json();
+        console.log(update);
+        return;
+    }
+
+    async insertCustomer(customer) {
+        const response = await fetch(`/admin_dashboard/index.php/api/customers/edit/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(customer[0])
         });
         let data = await response.json();
-        
-        if (data.success) {
-            let customer = await this.searchCustomer(data.customer_id);
-            return customer;
-        } else {
-            return false;
-        }
+
+        let customerId = await fetch(`/admin_dashboard/index.php/api/customers/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        customerId = await customerId.json();
+        customerId = customerId[customerId.length - 1].id;
+        return customerId;
     }
 }
